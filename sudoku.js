c431ed94ebbf9ab69ab29649781af19cdeeac298
Kirill Bulatov <Daffy_duck98@yahoo.com>
2022-12-17 17:48:56 +0300

    сделали проверку по вертикали на то , что не равно 45

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,8 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+const size = 9;
+
 function solve(boardString) {
   //   // let regexp = /(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})/gi
   //   // return boardString.replace(regexp, '$1,$2,$3,$4,$5,$6,$7,$8,$9').split(',')
@@ -35,6 +37,16 @@ function isSolved(board) {
   board.every((el) => {
     if (el.reduce((acc, elS) => acc + Number(elS), 0) !== 45) return false;
   });
+
+  for (let i = 0; i < size; i += 1) {
+    const result = [];
+    for (let q = 0; q < size; q += 1) {
+      result.push(board[q][i]);
+    }
+    if (result.reduce((a, b) => a + Number(b), 0) !== 45) {
+      return false;
+    }
+  }
 }
 // console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')))
 
