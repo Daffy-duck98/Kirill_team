913410b7f86592a012a87c95803ae4155b1052d2
Pavel <zabolotnikovpg2@mail.ru>
2022-12-18 12:21:47 +0300

    Box validation

--- sudoku.js
+++ sudoku.js
@@ -49,12 +49,12 @@ function validate(currPos, arrBoard, num, boxSize) {
   const [y, x] = currPos;
   //Проверка что число встречается в строке
   for (let i = 0; i < size; i++) {
+    //для проверки уникальности элемета в строке и не проверять текущее значение
     if (Number(arrBoard[i][x]) === num && i !== y) {
       return false;
     }
   }
 
-  
   //Проверка что число встречается в колонке 
   for (i=0; i<board.length, i+=1){
     //для проверки уникальности элемета в колонке и не проверять текущее значение
@@ -63,8 +63,22 @@ function validate(currPos, arrBoard, num, boxSize) {
     }
     }
   // Проверка что число встречается в секторе квадрата
+  //Проверяем инаходим начало квадрата, где левая верхняя ячейка
+  //Вычисляем в какой строке находится наш квадрат размер квадрата 3
+  const firstBlockOfBoxY = Math.floor(y / 3) * 3;
+  //Вычисляем в какой колонке находится наш квадрат  размер квадрата 3
+  const firstBlockOfBoxX = Math.floor(x / 3) * 3;
+  //Проводим проверку по квадрату 
+  for (let i = firstBlockOfBoxY; i < 3 + firstBlockOfBoxY; i++) {
+    for (let j = firstBlockOfBoxX; j < 3 + firstBlockOfBoxX; j++) {
+      if (Number(arrBoard[i][j]) === num && i !== y && j !== x) {
+        return false;
+      }
+    }
+  }
+  return true;
 
-}
+} 
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
