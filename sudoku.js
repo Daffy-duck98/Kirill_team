2d705dae715cacff467a35e7aef2fe4cfc07ff57
Pavel <zabolotnikovpg2@mail.ru>
2022-12-18 11:50:58 +0300

    Coll validation

--- sudoku.js
+++ sudoku.js
@@ -3,37 +3,37 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-//   // let regexp = /(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})/gi
-//   // return boardString.replace(regexp, '$1,$2,$3,$4,$5,$6,$7,$8,$9').split(',')
-  const regX = /.{9}/g;
-  const board = boardString.match(regX).map((el) => el.split(''));
+// function solve(boardString) {
+// //   // let regexp = /(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})\s*(.{9})/gi
+// //   // return boardString.replace(regexp, '$1,$2,$3,$4,$5,$6,$7,$8,$9').split(',')
+//   const regX = /.{9}/g;
+//   const board = boardString.match(regX).map((el) => el.split(''));
 
-  for (let i = 0; i < board.length; i += 1) {
-    for (let j = 0; j < board.length; j += 1) {
-      if (board[i][j] === '-') {
-        board[i][j] = (Math.floor(Math.random() * (10 - 1)) + 1).toString();
-        // return [i, j];
-      }
-    }
-  }
-  return board;
-}
+//   for (let i = 0; i < board.length; i += 1) {
+//     for (let j = 0; j < board.length; j += 1) {
+//       if (board[i][j] === '-') {
+//         board[i][j] = (Math.floor(Math.random() * (10 - 1)) + 1).toString();
+//         // return [i, j];
+//       }
+//     }
+//   }
+//   return board;
+// }
 // console.log(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--'));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-  board.map((arr) => arr.map((el) => {
-    if (el === '-') {
-      return false;
-    }
-    return true;
-  }));
-}
-console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')));
+// function isSolved(board) {
+//   board.map((arr) => arr.map((el) => {
+//     if (el === '-') {
+//       return false;
+//     }
+//     return true;
+//   }));
+// }
+// console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -41,6 +41,23 @@ console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
+
+}
+
+//Делаем проверку на валидность 
+function validate(currPos, arrBoard, num, boxSize) {
+  const [y, x] = currPos;
+  //Проверка что число встречается в строке
+  
+  
+  //Проверка что число встречается в колонке 
+  for (i=0; i<board.length, i+=1){
+    //для проверки уникальности элемета в колонке и не проверять текущее значение
+    if (Number(arrBoard[y][i]) === num && i !== x) { 
+      return false;
+    }
+    }
+  // Проверка что число встречается в секторе квадрата
 
 }
 
