8c440edfad66c6c137f474eab1325d66cbb20d8f
Pavel <zabolotnikovpg2@mail.ru>
2022-12-18 11:31:45 +0300

    Start validation

--- sudoku.js
+++ sudoku.js
@@ -9,34 +9,31 @@ function solve(boardString) {
   const regX = /.{9}/g;
   const board = boardString.match(regX).map((el) => el.split(''));
 
-  const findEmpty = (board) => {
-    for (let i = 0; i < board.length; i += 1) {
-      for (let j = 0; j < board.length; j += 1) {
-        if (board[i][j] === '-') {
-        // board[i][j] = (Math.floor(Math.random() * (10 - 1)) + 1).toString();
-          return [i, j];
-        }
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board.length; j += 1) {
+      if (board[i][j] === '-') {
+        board[i][j] = (Math.floor(Math.random() * (10 - 1)) + 1).toString();
+        // return [i, j];
       }
     }
-    return null;
-  };
+  }
+  return board;
 }
-console.log(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--'));
+// console.log(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--'));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-  const currPos = findEmpty(board);
-  // board.map((arr) => arr.map((el) => {
-  //   if (el === '-') {
-  //     return false;
-  //   }
-  //   return true;
-  // }));
+  board.map((arr) => arr.map((el) => {
+    if (el === '-') {
+      return false;
+    }
+    return true;
+  }));
 }
-// console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')))
+console.log(isSolved(solve('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
